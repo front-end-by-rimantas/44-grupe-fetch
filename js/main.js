465d823254f10878c03b2d1152338491f74273ea
Rimantas <belauzas@gmail.com>
2023-06-05 11:41:43 +0300

    Fetch

--- js/main.js
+++ js/main.js
@@ -3,12 +3,21 @@
 
 const ulDOM = document.querySelector('ul');
 
-const data = [];
+const data = fetch('https://front-end-by-rimantas.github.io/44-grupe-fetch/data/users.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        for (const user of data) {
+            console.log(user);
+        }
+    });
 
-let HTML = '';
 
-for (const item of data) {
-    HTML += `<li>${item.id}) ${item.text}</li>`;
-}
+// let HTML = '';
 
-ulDOM.innerHTML = HTML;
+// for (const item of data) {
+//     HTML += `<li>${item.id}) ${item.text}</li>`;
+// }
+
+// ulDOM.innerHTML = HTML;
