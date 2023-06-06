fb94fd25d4eefdd1af8bd9427d8603479c42dbd7
Rimantas <belauzas@gmail.com>
2023-06-06 11:54:34 +0300

    JSDoc

---
+++ index.js
@@ -0,0 +1,10 @@
+import { sum } from "./js/sum.js";
+import { renderUsersList } from "./js/users.js";
+
+// function sum(a: any, b: any): any
+
+/**
+ * Funkcijos grazinta suma
+ * @type {number}
+ */
+const rezultatas = sum(7, 5);
--- js/users.js
+++ js/users.js
@@ -1,3 +1,11 @@
+/**
+ * Pagal gautus vartotoju duomenis yra sugeneruojamas HTML.
+ * @param {Object[]} data Vartotoju duomenu sarasas.
+ * @param {string} data[].name Vartotojo vardas;
+ * @param {number} data[].age Vartotojo amzius metais;
+ * @param {boolean} data[].isMarried Vartotojo vedybinis statusas;
+ * @returns 
+ */
 export function renderUsersList(data) {
     const ulDOM = document.querySelector('ul');
     let HTML = '';
@@ -12,3 +20,13 @@ export function renderUsersList(data) {
 
     ulDOM.innerHTML = HTML;
 }
+
+/**
+ * Assign the project to a list of employees.
+ * @param {Object[]} employees - The employees who are responsible for the project.
+ * @param {string} employees[].name - The name of an employee.
+ * @param {string} employees[].department - The employee's department.
+ */
+Project.prototype.assign = function (employees) {
+    // ...
+};
