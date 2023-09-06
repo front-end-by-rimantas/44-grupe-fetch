8f1addf129bbceb0cc4e21712a7a3e33d2668fbc
Rimantas <belauzas@gmail.com>
2023-09-06 22:33:33 +0300

    Update

--- js/users.js
+++ js/users.js
@@ -20,13 +20,3 @@ export function renderUsersList(data) {
 
     ulDOM.innerHTML = HTML;
 }
-
-/**
- * Assign the project to a list of employees.
- * @param {Object[]} employees - The employees who are responsible for the project.
- * @param {string} employees[].name - The name of an employee.
- * @param {string} employees[].department - The employee's department.
- */
-Project.prototype.assign = function (employees) {
-    // ...
-};
